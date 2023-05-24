b32278bade885d5a7f8d4e4bc74211614e209e66
Ryan Miller <83258823+ides315@users.noreply.github.com>
2023-05-24 12:20:57 -0700

    Script for local server in Windows

---
+++ testserver.cmd
@@ -0,0 +1,6 @@
+@echo off
+echo You must have Python 3 installed to run this script.
+echo To view the site: navigate to "localhost:8000" in any web browser.
+echo To stop the server: interrupt the script by pressing Ctrl+C in this window while it has focus.
+echo.
+python -m http.server
